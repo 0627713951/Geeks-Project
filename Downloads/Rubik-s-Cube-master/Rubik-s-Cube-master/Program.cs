55d449978513f26c743e7a86c6361ddd2075547a
mpho <princessmpow@gmail.com>
2023-11-14 14:11:10 +0200

    first commit

---
+++ Downloads/Rubik-s-Cube-master/Rubik-s-Cube-master/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    static string[] fruits = new string[10];
+
+    public static void Main(string[] args)
+    {
+        GetUserInput();
+        DisplayFruits();
+    }
+
+    public static void GetUserInput()
+    {
+        Console.WriteLine("Please enter 10 different fruits: ");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Enter fruit #{i + 1}: ");
+            fruits[i] = Console.ReadLine();
+        }
+    }
+
+    public static void DisplayFruits()
+    {
+        Console.WriteLine("\nCollection of Fruits: ");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Fruit #{i + 1}: {fruits[i]}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Geeks-Project
 # Geeks-Project
+# Geeks-Project
